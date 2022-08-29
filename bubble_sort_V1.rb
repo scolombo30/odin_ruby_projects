d60cb33b49d53aceb75bc3055eafab36643719d6
Sebastian <scolombo30@studenti.uninsubria.it>
2022-08-29 16:18:01 +0200

    Add file bubblesortv2 e change name image result v1

--- bubble_sort_V1.rb
+++ bubble_sort_V1.rb
@@ -1,4 +1,4 @@
-def bubble_sort(array)
+def bubble_sort_v1(array)
   array.each_with_index do |loop, times |
       array.map.with_index do |num, i|
         array[i + 1].nil? ? break : nil
@@ -12,5 +12,5 @@ def bubble_sort(array)
 end
 
 array = [10,91,83,171,36,55,64,739,29,1]
-p bubble_sort(array)
+p bubble_sort_v1(array)
 #=> [1, 10, 29, 36, 55, 64, 83, 91, 171, 739]
