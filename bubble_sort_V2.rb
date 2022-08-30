4297835a6ae641a9e4cc01e0f9e585725bab7d37
Sebastian <scolombo30@studenti.uninsubria.it>
2022-08-30 08:46:12 +0200

    Implement v2 of the bubblesort

---
+++ bubble_sort_V2.rb
@@ -0,0 +1,17 @@
+def bubble_sort_v2(array)
+  for i in 0...array.length
+    sorted = true
+    for j in 0...(array.length - 1)
+      if array[j] > array[j + 1]
+        array[j], array[j + 1] = array[j + 1], array[j]
+        sorted = false
+      end
+    end
+    break if sorted
+  end
+  array
+end
+
+arr = [12,45,67,3,1,8,9,87,123]
+p bubble_sort_v2(arr)
+#=> [1, 3, 8, 9, 12, 45, 67, 87, 123]
